f33443a541c948b1af24e2a2239ca82c62224adb
EC2 Default User <koichan1996@icloud.com>
2023-02-11 05:30:28 +0000

    comment_finished

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -9,7 +9,7 @@ class Public::CommentsController < ApplicationController
   end
 
   def destroy
-    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
     redirect_to post_path(params[:post_id])
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_10_071552) do
+ActiveRecord::Schema.define(version: 2023_02_09_070302) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -111,7 +111,6 @@ ActiveRecord::Schema.define(version: 2023_02_10_071552) do
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
     t.boolean "is_deleted", default: false, null: false
-    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
