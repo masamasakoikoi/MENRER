9bf5dc48232101114b3b5f68432ae3629b9789f1
EC2 Default User <koichan1996@icloud.com>
2023-02-13 05:21:58 +0000

    layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.last_name = "ゲスト"
+      user.first_name = "太郎"
+      user.last_name_kana = "ゲスト"
+      user.first_name_kana = "タロウ"
+      
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
