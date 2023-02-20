4d2c14d0ecdb5a936860624984cfabb5be08077a
masamasakoikoi <koichan1996@icloud.com>
2023-02-20 08:48:28 +0000

    follow_asynchronization

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,13 +2,11 @@ class Public::RelationshipsController < ApplicationController
   #フォローする時
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
   end
   
   #フォロー外す時
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
   end
   
   #フォロー一覧
